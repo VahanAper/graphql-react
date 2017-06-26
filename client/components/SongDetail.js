88af5618b9e22d78022b5aa88d1ace7abbe047af
VahanAper <asryanvahandev@gmail.com>
2017-06-26 17:30:15 +0400

    fetch a song wiht song Query into SongDetails Component

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
+    const { song, loading } = this.props.data;
+
+    if (loading) {
+      return <div>Loading song...</div>;
+    }
+
     return (
-      <h3>Song Details</h3>
+      <h3>{song.title} Details</h3>
     );
   }
 }
 
-export default SongDetail;
+export default graphql(fetchSong, {
+  options: (props) => {
+    return {
+      variables: {
+        id: props.params.id,
+      },
+    };
+  },
+})(SongDetail);
