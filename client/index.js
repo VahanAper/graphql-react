2a0381f2a7d994f788343bd968c4b7bf381da367
VahanAper <asryanvahandev@gmail.com>
2017-06-26 11:43:54 +0400

    initial setup of SongCreate Component

--- client/index.js
+++ client/index.js
@@ -11,6 +11,7 @@ import { ApolloProvider } from 'react-apollo';
 
 import App from './components/App';
 import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
 
 const client = new ApolloClient({});
 
@@ -20,6 +21,7 @@ const Root = () => {
       <Router history={hashHistory}>
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
+          <Route path="song/new" component={SongCreate} />
         </Route>
       </Router>
     </ApolloProvider>
